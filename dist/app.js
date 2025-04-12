b8d916cdbb5692aaddcf7542d73f0afb508eeded
AsherAmCohen <154266987+AsherAmCohen@users.noreply.github.com>
2025-04-11 21:46:08 -0600

    Delete dist directory

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const server_1 = require("./server/server");
-const Server = new server_1.server();
-Server.execute();
